2c0cda3dc9b867d4e1a9401d73adf56840e6e999
chudamani kashyap <59866089+arjunksyp@users.noreply.github.com>
2020-01-14 15:45:03 +0530

    Add files via upload

---
+++ reverseDNAarjun.pl
@@ -0,0 +1,7 @@
+# calculate the reverse complement ofa strant of DNA 
+#!/user/bin/perl
+$DNA = 'AGCAAGGTCAGACG';
+print "here is DNA seq",$DNA,"\n";
+$revdna = reverse $DNA;
+print "reverse sequence:",$revdna,"\n";
+exit;
