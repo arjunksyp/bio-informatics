031989294b63dcdfae84daa784847a3171f6db2a
chudamani kashyap <59866089+arjunksyp@users.noreply.github.com>
2020-01-28 16:04:13 +0530

    Add files via upload

---
+++ lab2/pop.pl
@@ -0,0 +1,9 @@
+@bases=('A','C','G','T');
+$base1=pop@bases;
+print"here's the element remove from the end:";
+print $base1,"\n\n";
+print"here's the remaining array of bases:";
+print"@bases\n";
+print"========================";
+print"\n\n";
+
---
+++ lab2/shift.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+
+@bases=('A','C','G','T');
+$base2=shift @bases;
+print"here's an element removed from the beginning:";
+print $base2,"\n\n";
+print"here's the remaining array of bases:";
+print "@bases\n";
+print"========================";
+print "\n\n";
