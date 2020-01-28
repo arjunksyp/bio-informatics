47db7813f4210d808914377229e8f8fc57eb6f53
chudamani kashyap <59866089+arjunksyp@users.noreply.github.com>
2020-01-28 16:02:23 +0530

    Add files via upload

---
+++ lab3/splicing.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+@bases=('A','C','G','T');
+splice(@bases,2,0,'X');
+print"Here's the array with an element inserted after the second element:";
+print"@bases\n";
+print"====";
+print "\n\n";
+
+
